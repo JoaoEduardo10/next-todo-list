681ee8b84daeb03ca9aa4165636cebb6595de266
João Eduardo <joaoeduardoj920@gmail.com>
2023-03-16 13:07:54 -0300

    feat: adding bg-signIN-end-signUp.jp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <h1>Ola</h1>;
+  return (
+    <>
+      <h1>Ola</h1>
+      <Image
+        src={'/images/bg-signIN-end-signUp.jpg'}
+        alt={'bg-signIN-end-signUp'}
+        width={100}
+        height={100}
+      />
+    </>
+  );
 }
