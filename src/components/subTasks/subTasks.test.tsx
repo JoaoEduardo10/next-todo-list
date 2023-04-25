08140aa0ee17b57b24340a4177e6b2e947b45995
João Eduardo <joaoeduardoj920@gmail.com>
2023-04-25 19:44:00 -0300

    feat(subTasks): adding test unit

---
+++ src/components/subTasks/subTasks.test.tsx
@@ -0,0 +1,93 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { SubTasks } from '.';
+import { renderTheme } from '../../utils/render-theme';
+import exp from 'constants';
+
+describe('<SubTasks />', () => {
+  const setSybTasks = jest.fn();
+
+  it('should render a subTasks', () => {
+    renderTheme(<SubTasks clearInput={false} setSubTasks={setSybTasks} />);
+
+    const conteiner = screen.getByRole('listbox');
+    const conteinerList = screen.getByRole('list');
+    const inputs = screen.getByRole('listitem');
+    const button = screen.getByRole('button', {
+      name: 'Adicionar Nova SubTarefa',
+    });
+
+    expect(conteiner).toBeInTheDocument();
+    expect(conteinerList).toBeInTheDocument();
+    expect(inputs).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should add a new input', () => {
+    renderTheme(<SubTasks clearInput={false} setSubTasks={setSybTasks} />);
+
+    const inputs = screen.getAllByPlaceholderText('Nome da tarefa');
+    const button = screen.getByRole('button', {
+      name: 'Adicionar Nova SubTarefa',
+    });
+
+    expect(inputs.length).toEqual(1);
+    expect(button).toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    expect(screen.getAllByPlaceholderText('Nome da tarefa').length).toEqual(2);
+  });
+
+  it('should delete an input', () => {
+    renderTheme(<SubTasks clearInput={false} setSubTasks={setSybTasks} />);
+
+    const button = screen.getByRole('button', {
+      name: 'Adicionar Nova SubTarefa',
+    });
+
+    fireEvent.click(button);
+
+    const inputs = screen.getAllByPlaceholderText('Nome da tarefa');
+    const deleteInput = screen.getAllByLabelText('Delete SubTarefa');
+
+    expect(inputs.length).toEqual(2);
+    expect(deleteInput.length).toEqual(2);
+
+    fireEvent.click(deleteInput[0]);
+
+    expect(screen.getAllByLabelText('Delete SubTarefa').length).toEqual(1);
+  });
+
+  it('should get value from the inputs', () => {
+    renderTheme(<SubTasks clearInput={false} setSubTasks={setSybTasks} />);
+
+    const button = screen.getByRole('button', {
+      name: 'Adicionar Nova SubTarefa',
+    });
+
+    fireEvent.click(button);
+
+    const inputs: any = screen.getAllByPlaceholderText('Nome da tarefa');
+
+    fireEvent.change(inputs[0], { target: { value: 'test1' } });
+    fireEvent.change(inputs[1], { target: { value: 'test2' } });
+
+    expect(inputs[0].value).toEqual('test1');
+    expect(inputs[1].value).toEqual('test2');
+  });
+
+  it('should reset all inputs', () => {
+    renderTheme(<SubTasks clearInput={true} setSubTasks={setSybTasks} />);
+
+    const button = screen.getByRole('button', {
+      name: 'Adicionar Nova SubTarefa',
+    });
+
+    fireEvent.click(button);
+
+    const inputs: any = screen.getAllByPlaceholderText('Nome da tarefa');
+
+    expect(inputs[0].value).toEqual('');
+    expect(inputs[1].value).toEqual('');
+  });
+});
