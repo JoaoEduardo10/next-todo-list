5de1e1d3707534ac12d726499e6f34d3483bb8db
João Eduardo <joaoeduardoj920@gmail.com>
2023-04-26 12:11:32 -0300

    feat(createTask): create component

--- src/components/forms/createTask/index.tsx
+++ src/components/forms/createTask/index.tsx
@@ -1,6 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from './styles';
+
 import { AiOutlineClose } from 'react-icons/ai';
+import { Inpult } from '../../inpult/inpult';
+import { Button } from '../../button';
+import { SubTasks } from '../../subTasks';
+import { MessageError } from '../../messageError';
+
+import { TSubTasks, TTasks } from '../../../types';
+import { createTask } from '../../../utils/fecths';
+
+import { TSession } from '../../modal';
+import { useSession } from 'next-auth/react';
+
+import { useAppSelector, useAppDispatch } from '../../../app/hooks';
+import { setTasksInBoard } from '../../../app/features/Boards/boardSlice';
+import { Tasks } from '../../task/styles';
+import { Loading } from '../../loading';
 
 export type CreateTasksProps = {
   setShowCreateTask: React.Dispatch<React.SetStateAction<boolean>>;
@@ -13,15 +29,134 @@ export const CreateTasks = ({
   show,
   rendering,
 }: CreateTasksProps) => {
+  const { data: Session } = useSession() as TSession;
+  const dispatch = useAppDispatch();
+  const actualBoard = useAppSelector((store) => store.boards.actualBoard);
+  const [clearInput, setClearInput] = useState(false);
+  const [subTasks, setSubTasks] = useState<TSubTasks[]>([]);
+  const [valueTaskName, setValueTaskName] = useState('');
+  const [valueDescription, setValueDescription] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmitFormTask = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!valueTaskName) {
+      setErrorMessage('Adicione um nome a tasks');
+      setError(true);
+
+      return;
+    }
+
+    if (valueTaskName.length < 3) {
+      setErrorMessage('Adicione um nome a tasks com no minimo 3 caracteres');
+      setError(true);
+
+      return;
+    }
+
+    if (valueDescription && valueDescription.length < 6) {
+      setErrorMessage('adicione no minimo 6 letras a description');
+      setError(true);
+
+      return;
+    }
+
+    if (subTasks.length < 1) {
+      setErrorMessage('adicione no minimo 1 subTarefa');
+      setError(true);
+
+      return;
+    }
+
+    for (const task in subTasks) {
+      if (!subTasks[task].text) {
+        setErrorMessage(`adicone um text a subTarefa ${+task + 1}`);
+        setError(true);
+
+        return;
+      }
+    }
+
+    const formTask: TTasks = {
+      boardConnect: actualBoard.taskConnect,
+      text: valueTaskName,
+      subTasks: subTasks,
+      description: valueDescription ?? '',
+    };
+
+    setLoading(true);
+    const task = await createTask(Session.acessToken, formTask);
+    setLoading(false);
+    dispatch(setTasksInBoard(task));
+    console.log(task);
+
+    setSubTasks([]);
+    setValueDescription('');
+    setValueTaskName('');
+    setClearInput(true);
+    setShowCreateTask(false);
+  };
+
+  const handleCloseMenu = () => {
+    setValueDescription('');
+    setValueTaskName('');
+    setSubTasks([]);
+    setClearInput(true);
+    setShowCreateTask(false);
+  };
+
+  useEffect(() => {
+    if (error) {
+      const time = setTimeout(() => {
+        setError(false);
+        setErrorMessage('');
+      }, 4000);
+
+      return () => clearTimeout(time);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (clearInput) {
+      setClearInput(false);
+    }
+  }, [clearInput]);
+
   return (
     <S.ConteinerCreateTask show={show} rendering={rendering}>
-      <S.CreateTaskForm>
+      <S.CreateTaskForm onSubmit={handleSubmitFormTask}>
+        {rendering && <MessageError error={error} text={errorMessage} />}
+        {loading && <Loading />}
         <S.Heading>
           Adicionar nova tarefa{' '}
-          <span onClick={() => setShowCreateTask(false)}>
+          <span onClick={handleCloseMenu}>
             <AiOutlineClose aria-label="Close DynamicBoard" />
           </span>
         </S.Heading>
+        <S.ConteinerInput>
+          <S.Label>Nome da tarefa</S.Label>
+          <Inpult
+            placeholder="Por exemplo: faça uma pausa para café"
+            type="text"
+            onChange={setValueTaskName}
+            value={valueTaskName}
+          />
+        </S.ConteinerInput>
+        <S.ConteinerInput>
+          <S.Label>Descrição</S.Label>
+          <S.TextArea
+            onChange={({ target }) => setValueDescription(target.value)}
+            value={valueDescription}
+          />
+        </S.ConteinerInput>
+        <S.ConteinerInput>
+          <S.Label>SubTarefas</S.Label>
+          <SubTasks setSubTasks={setSubTasks} clearInput={clearInput} />
+        </S.ConteinerInput>
+        <Button>Criar tarefa</Button>
       </S.CreateTaskForm>
     </S.ConteinerCreateTask>
   );
