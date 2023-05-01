c064148c128fbd436f777944ea2c50219d7afb8c
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-01 11:42:30 -0300

    feat(subTasksCheckBox): addinf styles

---
+++ src/components/subTasksCheckbox/styles.ts
@@ -0,0 +1,52 @@
+import styled, { css } from 'styled-components';
+
+export const Conteiner = styled.div`
+  ${({ theme }) => css`
+    margin-top: ${theme.padding.extra_small};
+    display: flex;
+    flex-direction: column;
+  `}
+`;
+
+export const Heading = styled.h3`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.padding.small};
+    color: gray;
+    font-size: 1.6rem;
+  `}
+`;
+
+export const ConteinerInput = styled.div`
+  ${({ theme }) => css`
+    margin-bottom: 1rem;
+    padding: 1rem;
+    background-color: ${theme.colors.secondaryColor};
+    display: flex;
+    gap: ${theme.padding.small};
+    align-items: center;
+
+    &:hover {
+      background-color: ${theme.colors.purpleColor};
+      opacity: background-color 0.4;
+    }
+  `}
+`;
+
+export const Input = styled.input`
+  ${({ theme }) => css`
+    width: ${theme.padding.small};
+    height: ${theme.padding.small};
+    transform: scale(1.2);
+  `}
+`;
+
+export const Label = styled.label`
+  ${({ theme }) => css`
+    font-weight: bold;
+
+    ${Input}:checked ~ & {
+      text-decoration: line-through;
+      font-weight: 400;
+    }
+  `}
+`;
