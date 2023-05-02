b75f2a5265b30ae8bbf857023a92b4f6600a07e2
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-02 12:01:18 -0300

    refactory(utilsFaction): ajust function returnedSubTasksConcluded

--- src/utils/utilsFactions.ts
+++ src/utils/utilsFactions.ts
@@ -1,11 +1,13 @@
 import { TTasks } from '../types';
 
 const returnedSubTasksConcluded = (task: TTasks) => {
-  const allSubTaskConcluded = task.subTasks.filter((total) => {
-    return total.concluded == true;
-  });
+  const allSubTaskConcluded =
+    task.subTasks &&
+    task.subTasks.filter((subTask) => {
+      return subTask.concluded == true;
+    });
 
-  return allSubTaskConcluded.length;
+  return allSubTaskConcluded?.length;
 };
 
 export { returnedSubTasksConcluded };
