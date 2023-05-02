8cac92d25dcea6896ec96438df7b58d7ad3b7c33
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-02 11:28:07 -0300

    feat(subTasksModal): adding mock

---
+++ src/components/subTasksModal/mock.ts
@@ -0,0 +1,19 @@
+import { TSubTasks } from '@/src/types';
+
+export const mockSubTask: TSubTasks[] = [
+  {
+    text: 'test1',
+    concluded: false,
+    uuid: '1234',
+  },
+  {
+    text: 'test 2',
+    concluded: true,
+    uuid: '12345',
+  },
+  {
+    text: 'test 3',
+    concluded: false,
+    uuid: '123456',
+  },
+];
