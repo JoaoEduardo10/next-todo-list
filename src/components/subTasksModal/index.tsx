4399f51eacd2dc0d94a564633edb7449ae0725e5
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-03 11:09:52 -0300

    feat(subTaskModal): adding updateSubTask

--- src/components/subTasksModal/index.tsx
+++ src/components/subTasksModal/index.tsx
@@ -1,23 +1,44 @@
 import { useEffect, useState } from 'react';
 import * as S from './styles';
-import { TSubTasks } from '@/src/types';
+import { TSubTasks } from '../../types';
 import { returnedSubTasksConcluded } from '@/src/utils/utilsFactions';
 
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { setNewSubTaskConcluded } from '../../app/features/Boards/boardSlice';
+import { updateSubTask } from '../../utils/fecths';
+import { useSession } from 'next-auth/react';
+import { TSession } from '../modal';
 
 export type SubTasksModalProps = {
   subTasks: TSubTasks[];
 };
 
 export const SubTasksModal = ({ subTasks }: SubTasksModalProps) => {
+  const { data: Session } = useSession() as TSession;
   const dispatch = useAppDispatch();
   const task = useAppSelector((store) => store.task.actualTask);
+  const { tasks } = useAppSelector(
+    (store) => store.boards.actualBoardWithTasks,
+  );
   const [subTaskCheckBox, setSubTaskCheckBox] = useState<TSubTasks[]>([]);
 
   useEffect(() => {
     setSubTaskCheckBox([...subTasks]);
   }, [subTasks]);
+
+  useEffect(() => {
+    if (subTaskCheckBox.length > 0) {
+      const time = setTimeout(async () => {
+        await updateSubTask(
+          Session.acessToken,
+          subTaskCheckBox,
+          task._id as string,
+        );
+      }, 4000);
+
+      return () => clearTimeout(time);
+    }
+  }, [tasks]);
 
   const handleChangeInput = (
     event: React.ChangeEvent<HTMLInputElement>,
