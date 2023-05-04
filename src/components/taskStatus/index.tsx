49f28071872aa0e512dc00d92b0b399499e42a9c
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-04 12:05:07 -0300

    refactory(taskStatus): ajust component

--- src/components/taskStatus/index.tsx
+++ src/components/taskStatus/index.tsx
@@ -1,23 +1,55 @@
 import { useEffect, useState } from 'react';
 import * as S from './styles';
 
-export type TaskStatusProprs = {
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { setNewStatus } from '../../app/features/Boards/boardSlice';
+import { useSession } from 'next-auth/react';
+import { TSession } from '../modal';
+import { updateStatus } from '../../utils/fecths';
+
+export type TaskStatusProps = {
   status: 'pending' | 'progress' | 'concluded';
 };
 
-export const TaskStatus = ({ status }: TaskStatusProprs) => {
-  const [valueStatus, setValueStatus] = useState<typeof status>('pending');
+export const TaskStatus = ({ status }: TaskStatusProps) => {
+  const { data: Session } = useSession() as TSession;
+  const dispatch = useAppDispatch();
+  const task = useAppSelector((store) => store.task.actualTask);
+  const [valueStatus, setValueStatus] =
+    useState<TaskStatusProps['status']>('pending');
+  const [rendered, setRendered] = useState(false);
+
+  useEffect(() => {
+    setRendered(true);
+  }, []);
 
   useEffect(() => {
     setValueStatus(status);
   }, [status]);
 
+  useEffect(() => {
+    if (Session) {
+      const time = setTimeout(async () => {
+        await updateStatus(
+          Session.acessToken,
+          valueStatus,
+          task._id as TaskStatusProps['status'],
+        );
+      }, 2000);
+
+      return () => clearTimeout(time);
+    }
+  }, [status, valueStatus]);
+
   const handleChangeValueSelect = (
     event: React.ChangeEvent<HTMLSelectElement>,
   ) => {
-    const value = event.target.value as typeof status;
+    const value = event.target.value as TaskStatusProps['status'];
 
-    setValueStatus(value);
+    if (rendered) {
+      setValueStatus(value);
+      dispatch(setNewStatus({ status: value, task }));
+    }
   };
 
   return (
